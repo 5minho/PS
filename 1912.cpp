21029011b15a8223b3c33e823b2d6bbc839d376e
minomi <5minhominho@gmail.com>
2017-03-19 02:04:02 +0900

    1912

---
+++ 1912.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  TestXcodeProjs
+//
+//  Created by 오민호 on 2017. 3. 8..
+//  Copight © 2017년 오민호. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <vector>
+
+#define MAX 100000
+#define MIN -1001
+
+inline int max(int a, int b) {
+    return (a>b) ? a : b;
+}
+
+int n = 0;
+
+int main() {
+    scanf("%d",&n);
+    std::vector<int> arr(n);
+    for(int i = 0 ; i < n ; ++i)
+        scanf("%d",&arr[i]);
+    int ret = MIN;
+    int psum = 0;
+    for(int i = 0 ; i < n ; ++i) {
+        psum = max(0, psum) + arr[i];
+        ret = max(ret, psum);
+    }
+    printf("%d\n",ret);
+}
+
+
