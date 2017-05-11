a971e7cb00100b52cb0ba462744700c7a9b4dade
minomi <5minhominho@gmail.com>
2017-05-11 11:32:17 +0900

    solve 2178

---
+++ 2178.cpp
@@ -0,0 +1,67 @@
+//
+// Created by 오민호 on 2017. 5. 10..
+//
+
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <utility>
+
+const int MAX = 101;
+const int INF = 987654321;
+//int map[MAX][MAX];
+
+std::string map[MAX];
+int visited[MAX][MAX];
+
+int n,m;
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0 ,1, 0, -1};
+
+//struct pos {
+//    int y;
+//    int x;
+////    pos(int _y,int _x):y(_y),x(_x) {}
+//};
+
+
+int main() {
+    scanf("%d %d",&n,&m);
+    memset(visited, 0, sizeof(visited));
+    for(int i = 0 ; i < n ; ++i)
+        std::cin >> map[i];
+
+    for(int i = 0 ; i < n ; ++i)
+        for(int j = 0 ; j < m ; ++j)
+            visited[i][j] = INF;
+
+    std::queue<std::pair<int, int> > queue;
+    //pos originPoint = {0, 0};
+    queue.push(std::make_pair(0, 0));
+    visited[0][0] = 1;
+
+    while(!queue.empty()) {
+        std::pair<int, int> curPoint = queue.front();
+        queue.pop();
+
+        if(curPoint.first == n - 1 && curPoint.second == m - 1) {
+            printf("%d\n", visited[curPoint.first][curPoint.second]);
+            return 0;
+        }
+
+        for(int i = 0 ; i < 4 ; ++i) {
+            int ny = curPoint.first + dy[i];
+            int nx = curPoint.second + dx[i];
+            if(ny >= 0 && nx >= 0 && ny < n && nx < m && map[ny][nx] == 49) {
+                if(visited[ny][nx] > visited[curPoint.first][curPoint.second] + 1) {
+                    queue.push(std::make_pair(ny, nx));
+                    //printf("%d\n",visited[curPoint.y][curPoint.x], );
+                    visited[ny][nx] = visited[curPoint.first][curPoint.second] + 1;
+                }
+            }
+        }
+    }
+
+    printf("%d\n",-1);
+}
