c93808c8b94704b6b6267fbb8d1ea09a70302e08
minomi <5minhominho@gmail.com>
2017-06-24 15:23:18 +0900

    피보나치 수열 1,2

---
+++ DynamicProgramming/2748.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 오민호 on 2017. 6. 24..
+//
+
+//
+// Created by 오민호 on 2017. 6. 24..
+//
+
+#include <iostream>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    int n;
+    std::cin >> n;
+    long long seq[3] = {0, 1, };
+    for(int i = 2 ; i <= n ; ++i)
+        seq[i % 3] = seq[(i - 1) % 3] + seq[(i - 2) % 3];
+    std::cout << seq[n % 3];
+}
